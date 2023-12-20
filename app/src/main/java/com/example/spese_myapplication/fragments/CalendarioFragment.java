24ea9bd6490886743478d6971d778432155e7b7c
martinaminiello <94961059+martinaminiello@users.noreply.github.com>
2023-12-20 13:21:25 +0100

    Revert "RecycleViwer added but date is STILL NOT filtered"
    This reverts commit 9a098deedd60d90e5dc7ab28444ae3cde9f589d3.

--- app/src/main/java/com/example/spese_myapplication/fragments/CalendarioFragment.java
+++ app/src/main/java/com/example/spese_myapplication/fragments/CalendarioFragment.java
@@ -7,29 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Spinner;
-
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.spese_myapplication.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
 
 public class CalendarioFragment extends Fragment {
 
     private Button btnDatePicker;
     private Calendar selectedDate;
-    private List<Item> itemList = new ArrayList<>();
-    private MyAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
@@ -48,47 +39,6 @@ public class CalendarioFragment extends Fragment {
 
         // Update button text with the current date
         updateButtonText();
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-
-        adapter = new MyAdapter(itemList);
-        recyclerView.setAdapter(adapter);
-
-        FloatingActionButton btnAddItem = view.findViewById(R.id.btnAddItem);
-        EditText etNome = view.findViewById(R.id.editNome);
-        EditText etTipo = view.findViewById(R.id.editTipo);
-        EditText etPrezzo = view.findViewById(R.id.editPrezzo);
-
-        btnAddItem.setOnClickListener(view1 -> {
-            // Get user input
-            String nome = etNome.getText().toString();
-            String tipo = etTipo.getText().toString();
-            int prezzo = Integer.parseInt(etPrezzo.getText().toString());
-
-
-            // Create a new item with the selected date
-            Item newItem = new Item(nome, tipo, prezzo, selectedDate);
-
-            // Add the new item to the list
-            itemList.add(newItem);
-
-            // Clear the input fields
-            etNome.getText().clear();
-            etTipo.getText().clear();
-            etPrezzo.getText().clear();
-
-            // Filter items based on the selected date
-            List<Item> itemsForSelectedDate = new ArrayList<>();
-            for (Item existingItem : itemList) {
-                if (existingItem.hasSameDate(selectedDate)) {
-                    itemsForSelectedDate.add(existingItem);
-                }
-            }
-
-            // Update the adapter with the filtered list
-            adapter.setItems(itemsForSelectedDate);
-
-        });
 
         return view;
     }
@@ -121,5 +71,4 @@ public class CalendarioFragment extends Fragment {
         String buttonText = sdf.format(selectedDate.getTime());
         btnDatePicker.setText(buttonText);
     }
-
 }
